e934278e8a4dd2beefcd8bc75274a3df61898159
Mudit Ameta <zeusdeux@gmail.com>
2017-09-22 17:42:39 +0200

    Add support to look up author by name, refactor code
    Also, and add descriptions for all types to make for a better dev
    experience.

--- schema.js
+++ schema.js
@@ -1,21 +1,49 @@
 const fetch = require('node-fetch')
 const util = require('util')
 const parseXML = util.promisify(require('xml2js').parseString)
-const {goodreads:{key}} = require('./config')
+const {goodreads: {key}} = require('./config')
 
 const {
   GraphQLObjectType,
   GraphQLSchema,
   GraphQLInt,
   GraphQLString,
-  GraphQLList
+  GraphQLList,
+  GraphQLUnionType
 } = require('graphql')
 
 
-const getAuthor = id => fetch(`https://www.goodreads.com/author/show.xml?id=${id}&key=${key}`)
-      .then(r => r.text())
-      .then(parseXML)
-      .then(r => r.GoodreadsResponse.author[0])
+const fetchGR = (endpoint, q = '') => {
+  return fetch(`https://www.goodreads.com/${endpoint}?${q}${q.length ? '&' : ''}key=${key}`)
+    .then(r => r.text())
+    .then(parseXML)
+    .then(r => {
+      const { error, GoodreadsResponse } = r
+      if (error) {
+        return Promise.reject(new Error(error))
+      }
+
+      return GoodreadsResponse
+    })
+}
+const getAuthorById = id => {
+  return fetchGR('author/show.xml', `id=${id}`)
+    .then(r => r.author[0])
+}
+
+const getAuthorByName = name => {
+  const authorName = encodeURIComponent(name)
+
+  return fetchGR(`api/author_url/${authorName}`)
+    .then(r => {
+      try {
+        return r.author[0]['$'].id
+      } catch (_) {
+        return Promise.reject(new Error('author not found'))
+      }
+    })
+    .then(getAuthorById)
+}
 
 const BookType = new GraphQLObjectType({
   name: 'Books',
@@ -23,10 +51,12 @@ const BookType = new GraphQLObjectType({
   fields: _ => ({
     title: {
       type: GraphQLString,
+      description: 'Book title',
       resolve: books => books.title
     },
     isbn: {
       type: GraphQLString,
+      description: 'Book isbn number',
       resolve: books => books.isbn
     }
   })
@@ -34,25 +64,25 @@ const BookType = new GraphQLObjectType({
 
 const AuthorType = new GraphQLObjectType({
   name: 'Author',
-  description: 'Author name',
+  description: 'Author details',
   fields: _ => ({
     name: {
       type: GraphQLString,
+      description: 'Author name',
       resolve: parsedXml => parsedXml.name[0]
     },
     books: {
       type: new GraphQLList(BookType),
+      description: 'List of books by author',
       args: {
-        title: { type: GraphQLString }
+        title: { type: GraphQLString, description: 'Book title to search for. It is case insensitive and support regex.' }
       },
       resolve: (parsedXml, { title: titleArg }) => {
-        const booksList = parsedXml
-              .books[0]
-              .book
-              .map(({title: [title], isbn: [isbn]}) => ({title, isbn}))
+        const booksList = parsedXml.books[0].book.map(({title: [title], isbn: [isbn]}) => ({title, isbn}))
 
         if (titleArg) {
           const argAsRegex = new RegExp(titleArg, 'i') // TODO: sanitize user input i.e., titleArg
+
           return booksList.filter(({title}) => argAsRegex.test(title))
         }
 
@@ -70,10 +100,27 @@ module.exports = new GraphQLSchema({
     fields: _ => ({
       author: {
         type: AuthorType,
+        description: 'Author of a book. Supports search by id or author name',
         args: {
-          id: { type: GraphQLInt }
+          id: { type: GraphQLInt, description: 'Author id to search for' },
+          name: { type: GraphQLString, description: 'Author name to search for' }
         },
-        resolve: (root, { id }) => getAuthor(id)
+        resolve: (root, { id, name }) => {
+          const hasName = !!name
+          const hasId = typeof id === 'number'
+
+          if (hasName && hasId) {
+            return Promise.reject(new Error('Provide either author name or author id, not both'))
+          }
+          if (hasName) {
+            return getAuthorByName(name)
+          }
+          if (hasId) {
+            return getAuthorById(id)
+          }
+
+          return Promise.reject(new Error('Author id or name argument must be provided'))
+        }
       }
     })
   })
